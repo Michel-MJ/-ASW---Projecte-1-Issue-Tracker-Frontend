5793d4ea8bed0b1d2ed5fdd7af0a2f53dc8b89f7
Roger Guinovart <roger.guinovart@estudiantat.upc.edu>
2026-05-21 00:05:51 +0200

    Index issues + bulk issues + filtres no acabats del tot

--- src/router/index.js
+++ src/router/index.js
@@ -12,8 +12,8 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: '/issues',
+      name: 'index',
       component: IssueList
     },
     {
